0410bede7fc1ee516a4e675c8c8196d9d92f26ff
ElPapi42 <whitman-2@hotmail.com>
2020-05-14 16:47:39 -0400

    Added upload feature to basic page

--- src/DeblurPage/index.vue
+++ src/DeblurPage/index.vue
@@ -1,11 +1,39 @@
 <template>
-    <div>
-        <h1>Hello Deburring</h1>
+    <div class="flex flex-col items-center">
+        <input type="file" id="inputImage" ref="inputImage" v-on:change="handleImageUpload()"/>
+        <button v-on:click="submitImage()">Submit</button>
     </div>
 </template>
 
 <script>
     export default {
         name: 'DeblurPage',
+
+        data: function () {
+            return {
+                imageFile: '',
+            }
+        },
+
+        methods: {
+            handleImageUpload() {
+                this.imageFile = this.$refs.inputImage.files[0];
+            },
+
+            submitImage() {
+                let formData = new FormData();
+                formData.append('image', this.imageFile);
+
+                this.$http.post(
+                    'http://localhost:8000/api/inference/',
+                    formData,
+                    { headers: { 'Content-Type': 'multipart/form-data' }},
+                ).then(function(response) {
+                    console.log(response);
+                }).catch(function(error) {
+                    console.log(error);
+                });
+            },
+        },
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 import './main.css'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
   render: h => h(App),
