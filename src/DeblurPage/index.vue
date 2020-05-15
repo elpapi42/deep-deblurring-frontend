9f89d861ed0184ba915ca4e99dd271e8c3b03984
ElPapi42 <whitman-2@hotmail.com>
2020-05-14 23:46:26 -0400

    Barebones of inference UI

--- src/DeblurPage/index.vue
+++ src/DeblurPage/index.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="flex flex-col items-center">
+        <img v-if="input_url" :src="input_url"/>
+        <img v-if="output_url" :src="output_url"/>
         <input type="file" id="inputImage" ref="inputImage" v-on:change="handleImageUpload()"/>
         <button v-on:click="submitImage()">Submit</button>
     </div>
@@ -12,15 +14,19 @@
         data: function () {
             return {
                 imageFile: '',
+                input_url: null,
+                output_url: null,
             }
         },
 
         methods: {
             handleImageUpload() {
                 this.imageFile = this.$refs.inputImage.files[0];
+                this.input_url = URL.createObjectURL(this.imageFile)
             },
 
             submitImage() {
+                let self = this;
                 let formData = new FormData();
                 formData.append('image', this.imageFile);
 
@@ -29,7 +35,7 @@
                     formData,
                     { headers: { 'Content-Type': 'multipart/form-data' }},
                 ).then(function(response) {
-                    console.log(response);
+                    self.output_url = response.data.output_image;
                 }).catch(function(error) {
                     console.log(error);
                 });
