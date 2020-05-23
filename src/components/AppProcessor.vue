791464a9d7cb0e439b781239a096d2459f39a806
ElPapi42 <whitman-2@hotmail.com>
2020-05-23 18:25:54 -0400

    Improve fluid design removing some width constrainst to the image processor

--- src/components/AppProcessor.vue
+++ src/components/AppProcessor.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class='flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0 max-w-screen-sm w-full justify-center'>
-        <div class='h-64 sm:h-full w-full sm:w-64'>
+    <div class='flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0 max-w-screen-md w-full justify-center'>
+        <div class='h-64 sm:h-full w-full'>
             <image-uploader @load='onLoad' @error='onError' @upload='onUpload'/>
         </div>
-        <div class='h-64 sm:h-full w-full sm:w-64'>
+        <div class='h-64 sm:h-full w-full'>
             <image-downloader :src='outputUrl' :name='imageName' :loading='loading'/>
         </div>
     </div>
