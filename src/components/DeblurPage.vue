a7e2c0a53b9a793ee191df31ca1c0ea54a8c0939
ElPapi42 <whitman-2@hotmail.com>
2020-05-25 15:17:53 -0400

    Add shadows to stack box, header, and processor

--- src/components/DeblurPage.vue
+++ src/components/DeblurPage.vue
@@ -1,7 +1,7 @@
 <template>
 <div class='flex flex-col space-y-4'>
     <!--Heading-->
-    <header class='bg-gray-500 justify-center'>
+    <header class='shadow-md justify-center'>
         <h1 class='text-2xl text-center p-2'>Deep\PNG</h1>
     </header>
 
