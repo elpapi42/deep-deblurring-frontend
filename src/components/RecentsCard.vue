02634c56e738185dc01d8e369b6890c954c7ee2d
ElPapi42 <whitman-2@hotmail.com>
2020-05-26 16:02:52 -0400

    Add rating component and integrates it to the recents cards

--- src/components/RecentsCard.vue
+++ src/components/RecentsCard.vue
@@ -1,19 +1,19 @@
 <template>
     <div class='flex flex-col space-y-2 pb-2 items-center bg-gray-300 h-full'>
         <image-downloader :src='data.url' :name='data.name'/>
-        <star-rating item-size='12' :show-rating='false'></star-rating>
+        <app-rating :uid='data.uuid' :size='12'/>
     </div>
 </template>
 
 <script>
-import {StarRating} from 'vue-rate-it';
+import AppRating from './AppRating'
 import ImageDownloader from './ImageDownloader';
 
 export default {
     name: 'RecentsCard',
 
     components:{
-        StarRating,
+        AppRating,
         ImageDownloader,
     },
 
