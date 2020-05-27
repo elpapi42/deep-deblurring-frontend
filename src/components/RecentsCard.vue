c3cc224f7afea209a1e19c7243db20f43bd51869
ElPapi42 <whitman-2@hotmail.com>
2020-05-27 13:10:58 -0400

    Add hover animation to recents cards

--- src/components/RecentsCard.vue
+++ src/components/RecentsCard.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class='flex flex-col space-y-2 pb-2 items-center bg-gray-300 h-full'>
+    <div 
+        class='flex flex-col space-y-2 pb-2 items-center bg-gray-300 h-full' 
+        v-bind:class='{ "hover:opacity-75 hover:shadow-2xl": !loading }'
+    >   
         <image-downloader :src='data.url' :name='data.name'/>
         <app-rating :rating='data.score' :uid='data.uuid' :size='12'/>
     </div>
