79b69c09973786fc53aff986ccdcbc239d7a4c11
ElPapi42 <whitman-2@hotmail.com>
2020-05-28 13:48:30 -0400

    Add border to recents cards

--- src/components/RecentsCard.vue
+++ src/components/RecentsCard.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class='flex flex-col space-y-2 pb-2 items-center bg-gray-300 h-full'>
+    <div class='flex flex-col space-y-2 pb-2 items-center rounded border-2 border-gray-700 h-full'>
         <image-downloader 
             :src='data.url' 
             :name='data.name' 
-            class='hover:opacity-75 hover:shadow-2xl cursor-pointer'
+            class='hover:opacity-75 cursor-pointer'
         />
         <app-rating :rating='data.score' :uid='data.uuid' :size='12'/>
     </div>
