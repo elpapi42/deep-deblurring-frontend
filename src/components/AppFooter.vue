822952fcdf9712c13e56de04783df8bc9e442477
ElPapi42 <whitman-2@hotmail.com>
2020-05-30 10:46:09 -0400

    Add links to all the resources in the footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,17 +2,41 @@
     <div class='grid grid-cols-2'>
         <ul class='flex flex-col'>
             <li class='text-white text-xl mb-2'>About DeepPNG</li>
-            <li class='text-white'>Github</li>
-            <li class='text-white'>LinkedIn</li>
+            <li class='text-white'>Github:
+                <a class='text-teal-300' href='https://github.com/ElPapi42/deep-deblurring'>
+                    ElPapi42/deep-deblurring
+                </a>
+            </li>
+            <li class='text-white'>LinkedIn:
+                <a class='text-teal-300' href='https://www.linkedin.com/in/whitmanb/'>
+                    deeppng
+                </a>
+            </li>
         </ul>
         <ul class='flex flex-col items-end'>
             <li class='text-right text-white text-xl mb-2'>About Whitman Bohorquez</li>
-            <li class='text-right text-white'>Github</li>
-            <li class='text-right text-white'>LinkedIn</li>
-            <li class='text-right text-white'>Resume</li>
-            <li class='text-right text-white'>Email</li>
+            <li class='text-right text-white'>Github: 
+                <a class='text-teal-300' href='https://github.com/ElPapi42'>
+                    ElPapi42
+                </a>
+            </li>
+            <li class='text-right text-white'>LinkedIn:
+                <a class='text-teal-300' href='https://www.linkedin.com/in/whitmanb/'>
+                    whitmanb
+                </a>
+            </li>
+            <li class='text-right text-white'>Resume: 
+                <a class='text-teal-300' href='https://drive.google.com/file/d/1L01THIGwKjbRR_6L2Nwlr99G1i9OQZEs/view'>
+                    PDF
+                </a>
+            </li>
+            <li class='text-right text-white'>Email:
+                <a class='text-teal-300' href='mailto: whitman-2@hotmail.com'>
+                    whitman-2@hotmail.com
+                </a>
+            </li>
         </ul>
-        <h1 class='col-span-2 text-white align-bottom'>DeepPNG. All Rights Not Reserved</h1>
+        <h1 class='col-span-2 text-white align-bottom text-center sm:text-left mt-4'>DeepPNG. All Rights Not Reserved</h1>
     </div>
 </template>
 
